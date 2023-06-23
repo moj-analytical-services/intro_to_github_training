74e2edde3b6b24f28c654269b69a7e172e944c09
Ian Rickard <ian.rickard@justice.gov.uk>
2023-06-23 11:13:08 +0000

    Cristiano adding two

--- Collaboration/Collaborative_R_script.R
+++ Collaboration/Collaborative_R_script.R
@@ -1,5 +1,11 @@
-# This is a simple function that adds one to a numeric input value
+# This adds one
 add_one <- function(value) {
   new_value <- value + 1
+  return(new_value)
+}
+
+# This adds two
+add_two <- function(value) {
+  new_value <- value + 2
   return(new_value)
 }

ef78d6e9c920eb0f349427fc5e67206059fb978a
Ian Rickard <ian.rickard@justice.gov.uk>
2023-06-23 11:17:26 +0000

    annotation changes

--- Collaboration/Collaborative_R_script.R
+++ Collaboration/Collaborative_R_script.R
@@ -1,10 +1,10 @@
-# This adds one
+# Function to add one
 add_one <- function(value) {
   new_value <- value + 1
   return(new_value)
 }
 
-# This adds two
+# Function to add two
 add_two <- function(value) {
   new_value <- value + 2
   return(new_value)

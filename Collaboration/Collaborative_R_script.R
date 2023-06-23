98b1284a8231377e24abc45d60530d71c7d55b3a
Ian Rickard <ian.rickard@justice.gov.uk>
2023-06-23 11:34:02 +0000

    reverting

--- Collaboration/Collaborative_R_script.R
+++ Collaboration/Collaborative_R_script.R
@@ -1,9 +1,4 @@
-# This function adds one to a numeric input
 add_one <- function(value) {
-  
   new_value <- value + 1
-  
-  print(paste("adding 1 to the input value: ", value))
-
   return(new_value)
 }

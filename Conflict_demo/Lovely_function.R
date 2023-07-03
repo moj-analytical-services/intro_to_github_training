e0bdc01b7a35f98a7a0b4af3c0052aff589550c4
Ian Rickard <ian.rickard@justice.gov.uk>
2023-07-03 13:01:16 +0000

    conflict setup

--- Conflict_demo/Lovely_function.R
+++ Conflict_demo/Lovely_function.R
@@ -2,12 +2,12 @@
 add_one <- function(value) {
   
   if (class(value) != 'numeric') {
-    stop("Please provide a numeric input")
+    stop("This function accepts numeric input only")
   }
 
   new_value <- value + 1
   
-  print(paste("Adding 1 to your input value: ", value))
+  print(paste("Now adding 1 to the value you input: ", value))
   
   return(new_value)
   

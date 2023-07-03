bf4202054be26537c865cffd12c28a5cd7049854
Ian Rickard <ian.rickard@justice.gov.uk>
2023-07-03 13:02:39 +0000

    change to annotation and parameter name

--- Conflict_demo/Lovely_function.R
+++ Conflict_demo/Lovely_function.R
@@ -1,14 +1,14 @@
-# This function adds one to a numeric input
+# This is a function to add one to a numeric input
 add_one <- function(value) {
   
   if (class(value) != 'numeric') {
     stop("Please provide a numeric input")
   }
 
-  new_value <- value + 1
+  result <- value + 1
   
   print(paste("Adding 1 to your input value: ", value))
   
-  return(new_value)
+  return(result)
   
 }

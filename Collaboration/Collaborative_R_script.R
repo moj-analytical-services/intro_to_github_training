e416258b05b9b1211a3b288f237dadc21d9aacca
Ian Rickard <ian.rickard@justice.gov.uk>
2023-07-03 10:07:36 +0000

    conflict

--- Collaboration/Collaborative_R_script.R
+++ Collaboration/Collaborative_R_script.R
@@ -1,14 +1,14 @@
-# This function adds one to a numeric input
+# This is a function which adds one to a numeric input
 add_one <- function(value) {
   
   if (class(value) != 'numeric') {
     stop("this function only accepts numeric inputs")
   }
 
-  new_value <- value + 1
+  result <- value + 1
   
   print(paste("adding 1 to your input: ", value))
   
-  return(new_value)
+  return(result)
   
 }

bd33f9c65ca312dfa3d3c52b9a2fb54b11b23619
gary.mcelroy@justice.gov.uk <gary.mcelroy@justice.gov.uk>
2026-03-12 11:41:24 +0000

    Add renv

--- practice_folder/gary_practice_jul-25/gary-test-script.R
+++ practice_folder/gary_practice_jul-25/gary-test-script.R
@@ -1,2 +1,26 @@
+# Install packages
+install.packages("renv")
+renv::init()
 
-my_name <- "Gary"
+# Create a list
+my_list <- seq(from = 1, to = 30, by = 1)
+
+for (n in my_list) {
+  if ((n %% 3 == 0) & (n %% 5 == 0)) {
+    print("FizzBuzz")
+  } else if (n %% 3 == 0) {
+    print("Fizz")
+  } else if (n %% 5 == 0) {
+    print("Buzz")
+  } else {
+    print(n)
+  }
+}
+
+# Test while loop
+my_variable <- 0
+
+while (my_variable < 0) {
+  print("Test")
+  my_variable <- as.integer(my_variable)+1
+}
